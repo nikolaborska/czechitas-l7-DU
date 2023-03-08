dc6610058e2bb39fac41de76c6f5ca6ede2a1085
Nikola Borská <77294537+nikolaborska@users.noreply.github.com>
2023-03-08 07:54:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -20,9 +20,9 @@ const taxed = (salary, taxPercentage) => {
 	return Math.round((salary * (100 - taxPercentage)) / 100)
 };
 
-const wage = 400;
-const hours = 6;
-const days = 21;
+const wage = 150;
+const hours = 12;
+const days = 16;
 const beforeTaxation = salary(wage, hours, days);
 const afterTaxation = taxed(beforeTaxation, 15);
 
